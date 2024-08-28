503bec2bcad315165d2d42d2b0d5669d7357fc75
Enrique Madrid <marlonenrique100@gmail.com>
2024-08-27 21:48:43 -0600

    FIx tools_output calls_id

--- easy_bot/openai_conn.py
+++ easy_bot/openai_conn.py
@@ -55,7 +55,7 @@ class OpenAICore(AICore):
         content=content
       )
     
-      event_handler = EventHandler(self.__client, self.__thread)
+      event_handler = EventHandler(self.__client)
 
       with self.__client.beta.threads.runs.stream(
         thread_id=self.__thread.id,
@@ -91,33 +91,28 @@ class OpenAICore(AICore):
 
 class EventHandler(AssistantEventHandler):
   snapshot: Text
-  run_id: str
-  tool_outputs: list
   __last_instance: Optional['EventHandler']
 
-  def __init__(self, client: OpenAI, thread: Thread, last_instance: Optional['EventHandler'] = None,  tool_outputs: Optional[list] = None):
+  def __init__(self, client: OpenAI, last_instance: Optional['EventHandler'] = None):
     super().__init__()
     self.__client = client
-    self.__thread = thread
     self.snapshot = Text(annotations=[], value='')
     if last_instance is None:
       self.__last_instance = self
     else:
       self.__last_instance = last_instance
 
-    if tool_outputs is None: tool_outputs = []
-    self.tool_outputs = tool_outputs
-
   @override
   def on_text_created(self, text) -> None:
-    print(f"\nassistant > ", end="", flush=True)
+    # print(f"\nassistant_tx > ", end="", flush=True)
+    ...
       
   @override
   def on_text_delta(self, delta, snapshot):
     self.snapshot = snapshot
       
   def on_tool_call_created(self, tool_call):
-    print(f"\nassistant > {tool_call.type}\n", flush=True)
+    # print(f"\nassistant_to > {tool_call.type}\n", flush=True)
     ...
   
   def on_tool_call_delta(self, delta, snapshot):
@@ -126,12 +121,13 @@ class EventHandler(AssistantEventHandler):
   @override
   def on_event(self, event):
     if event.event == 'thread.run.requires_action':
-      self.run_id = event.data.id
-      self.handle_requires_action(event.data, self.run_id)
+      run_id = event.data.id
+      self.handle_requires_action(event.data, run_id)
 
   def handle_requires_action(self, data, run_id):
     from .easy_bot import EasyBot
-    
+    tool_outputs: list = []
+
     for tool in data.required_action.submit_tool_outputs.tool_calls:
       name_func: str = tool.function.name
       arguments: str = tool.function.arguments
@@ -141,20 +137,20 @@ class EventHandler(AssistantEventHandler):
       if name_func in EasyBot.funcs:
         func = EasyBot.funcs[name_func]
         output = func(**args_dic)
-        self.tool_outputs.append({"tool_call_id": tool.id, "output": str(output)})
+        tool_outputs.append({"tool_call_id": tool.id, "output": str(output)})
 
-        result = ''
-        with self.__client.beta.threads.runs.submit_tool_outputs_stream(
-          thread_id=self.__thread.id,
-          run_id=run_id,
-          tool_outputs=self.tool_outputs,
-          event_handler=EventHandler(self.__client, self.__thread, self.__last_instance)
-        ) as stream:
-          for text in stream.text_deltas:
-            result += text
+    result: str = self.submit_tool_outputs(tool_outputs)
+    if len(result) == 0: return
+    self.__last_instance.snapshot.value = result
 
-        if len(result) <= 0:
-          return
-
-        if self.__last_instance is None: return
-        self.__last_instance.snapshot.value = result
+  def submit_tool_outputs(self, tool_outputs):
+      content = ''
+      with self.__client.beta.threads.runs.submit_tool_outputs_stream(
+        thread_id=self.current_run.thread_id,
+        run_id=self.current_run.id,
+        tool_outputs=tool_outputs,
+        event_handler=EventHandler(self.__client, self.__last_instance)
+      ) as stream:
+        for text in stream.text_deltas:
+          content += text
+      return content
