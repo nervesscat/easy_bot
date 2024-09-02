3472bc5eb62c5a51fe519c385ad770bdf0058545
Enrique Madrid <marlonenrique100@gmail.com>
2024-09-01 22:09:35 -0600

    Refactorized code, and signature method moved

--- easy_bot/easy_bot.py
+++ easy_bot/easy_bot.py
@@ -1,7 +1,7 @@
-import inspect
+from .tools.tools import obtain_sig
 from .ai_cores import AICore
 from .openai_conn import OpenAICore
-from typing import Optional, Type, Callable
+from typing import Type, Callable
 from .types.easy_bot_types import FunctionSchema
 
 class AssistantNotCreated(Exception):
@@ -32,7 +32,7 @@ class EasyBot:
         self.__default_core_class = ai_core.__class__
 
     def add_function(self, func: Callable) -> None:
-        func_info: FunctionSchema = self.__obtain_sig(func)
+        func_info: FunctionSchema = obtain_sig(func)
         self.__functions_info.append(func_info)
         EasyBot.funcs[func_info["func_name"]] = func
         self.create_assistant()
@@ -47,38 +47,4 @@ class EasyBot:
             raise AssistantNotCreated("AI core isn't initialized")
         return self.__ai_core.create_image_completion(task, encoded_img)
 
-    def __obtain_sig(self, func: Callable) -> FunctionSchema:
-        types: dict = {
-            int: 'number',
-            str: 'string',
-            bool: 'boolean'
-        }
-
-        if not callable(func):
-            raise TypeError(f"The provided func for '{name}' is not callable")
-
-        func_name: str = func.__name__
-        func_doc: str | None = func.__doc__
-        if func_doc is None: func_doc = ''
-        
-        sig = inspect.signature(func)
-        parameters: list = []
-        required: list = []
-
-        for name, param in sig.parameters.items():
-            if param.annotation not in {int, str, bool}:
-                raise TypeError(f"Invalid type for parameter '{name}': {param.annotation.__name__}")
-
-            parameters.append((name, types[param.annotation]))
-
-            if param.default is inspect.Parameter.empty:
-                required.append((name, types[param.annotation]))
-
-        func_info: FunctionSchema = {
-            "func_name": func_name,
-            "func_doc": func_doc,
-            "parameters": parameters,
-            "required": required
-        }
-
-        return func_info
+    
